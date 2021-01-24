abf337a8225482152b0004806be50c10af42eb4e
PratilipiAich <pratilipiaich98@gmail.com>
2021-01-24 17:51:00 +0530

    Added Web Tech Lab Programs

---
+++ Web_Tech_Lab/Prog7.php
@@ -0,0 +1,17 @@
+<head>
+	<meta http-equiv="refresh" content="1"/>
+	<style>
+		p {
+			color:white;
+			font-size:90px;
+			position:absolute;
+			top: 30%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+		body {
+			background-color:#075177;
+		}
+	</style>
+	<p> <?php echo date(" h : i : s A");?> </p>
+</head> 
